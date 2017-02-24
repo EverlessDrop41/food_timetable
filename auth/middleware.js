c9f206e48d9fad2d299f6ad5fdb062e9d72f74d8
emilyperegrine <peregrineemily@gmail.com>
2017-02-24 23:47:19 +0000

    fixed issue when no credentials present

--- auth/middleware.js
+++ auth/middleware.js
@@ -15,14 +15,20 @@ module.exports = {
   require_admin: function (req, res, next) {
     var credentials = b_auth(req);
 
-    u_auth.isAdmin(credentials.name, credentials.pass, function (result, err) {
-      if (result) {
-        req.credentials = credentials;
-        req.is_admin = result;
-        next();
-      } else {
-        res.sendStatus(401);
-      }
-    });
+    if (credentials && credentials.name && credentials.pass) {
+      u_auth.isAdmin(credentials.name, credentials.pass, function (result, err) {
+        if (result) {
+          req.credentials = credentials;
+          req.is_admin = result;
+          next();
+        } else {
+          res.sendStatus(401);
+        }
+      });
+    } else {
+      res.sendStatus(401);
+    }
+
+    
   }
 }
