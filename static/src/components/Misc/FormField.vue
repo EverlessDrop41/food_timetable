821a827552b7c04cb2f2d6aae79607d8676d5b0e
emilyperegrine <peregrineemily@gmail.com>
2018-07-27 18:34:08 +0100

    updated to try fix security

---
+++ static/src/components/Misc/FormField.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="form-group">
+    <label for="confirmPasswordInput">{{ label }}</label>
+    <input type="password" class="form-control" id="confirmPasswordInput" 
+      maxlength="255" placeholder="{{ placeholder }}" require v-model="confirmPassword">
+  </div>
+</template>
+
+<script>
+</script>
